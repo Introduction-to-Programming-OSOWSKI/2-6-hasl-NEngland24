1ed3f9449583fee510f594e88929ed2c4b57533e
NEngland24 <824864@mystma.org>
2023-03-29 15:05:35 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(word):
+    numL= 0
+    for i in range(0,len):
+        if word[i] == "l":
+            numL += 1
+    if numL > 1:
+        return True
+    else:
+        False
